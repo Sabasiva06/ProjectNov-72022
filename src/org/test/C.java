27a29c34ec42bc13fe890f52ecdc7f36251bd9f9
Sabasiva06 <sabasiva06@gmail.com>
2022-11-09 13:28:21 +0530

    messages

--- src/org/test/C.java
+++ src/org/test/C.java
@@ -1,12 +1,23 @@
 package org.test;
 
-
+import java.util.Set;
+import java.util.TreeSet;
 
 public class C {
 
 	public static void main(String[] args) {
 
-		
+		Set<Integer> si = new TreeSet<>();
+		si.add(1);
+		si.add(2);
+		si.add(3);
+		si.add(4);
+		si.add(5);
+		si.add(6);
+
+		for (Integer integer : si) {
+			System.out.println(integer);
+		}
 
 	}
 
