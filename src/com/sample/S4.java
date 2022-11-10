a294ca34ba86a2cf901ad22461eb4242c762ed2b
rajanmw77 <rajansathi.rajan@gmail.com>
2022-11-10 11:14:32 +0530

    NOV_10

---
+++ src/com/sample/S4.java
@@ -0,0 +1,33 @@
+package com.sample;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+public class S4 {
+
+	public static void main(String[] args) {
+
+		List<String> li = new ArrayList<String>();
+		li.add("Bala");
+		li.add("bala@gmail.com");
+		li.add("Selenium");
+		li.add("Automation");
+		li.add("Greens");
+		li.add("OMT");
+		// {null=b,1=null,2=null}
+
+		Map<Integer, String> mp = new Hashtable<Integer, String>();
+		
+		// To add a Data in Map
+		mp.put(2, "A");
+		mp.put(5, null);
+		mp.put(1, null);
+		mp.put(6, "b");
+		mp.put(2, null);
+
+		System.out.println(li);
+
+	}
+}
